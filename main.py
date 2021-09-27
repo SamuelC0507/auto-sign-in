942fcb8fb9d26f1ce87b081cadd8758d8e4a7bfc
Samuel Chang <oldchang0507@gmail.com>
2021-09-28 01:30:51 +0800

    Able to select ROI and auto click the link

--- main.py
+++ main.py
@@ -1,44 +1,65 @@
 import tkinter as tk
 import time
+import threading
 import numpy as np
 import cv2
 import mss
 import mss.tools
-import webbrowser
-import pyautogui
+from pynput import mouse
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = "C:\\Sam\\Program_Files\\Tesseract-OCR\\tesseract.exe"
+# pytesseract.pytesseract.tesseract_cmd = "D:\\Sam\\Program_files\\Tesseract-OCR\\tesseract.exe"
+
+
+def on_click(x, y, button, pressed):
+    global bounding_box
+
+    if button.name == "right":
+        if pressed:
+            bounding_box["left"] = x
+            bounding_box["top"] = y
+        else:
+            bounding_box["width"] = abs(x - bounding_box["left"])
+            bounding_box["left"] = min(bounding_box["left"], x)
+            bounding_box["height"] = abs(y - bounding_box["top"])
+            bounding_box["top"] = min(bounding_box["top"], y)
+
+            btn_select_roi.config(state=tk.NORMAL, relief=tk.RAISED)
+            print_log("ROI: " + str(list(bounding_box.values())))
+
+            show_roi_thread = threading.Thread(target=show_roi, daemon=True)
+            show_roi_thread.start()
+            return False
 
 
 def select_roi():
-    sct = mss.mss()
-    monitor_number = 2
-    mon = sct.monitors[monitor_number]
-    bounding_box = {'top': mon["top"] + 1600, 'left': mon["left"] + 3200, 'width': 600, 'height': 200, 'mon': monitor_number}
+    btn_select_roi.config(state=tk.DISABLED, relief=tk.SUNKEN)
+    print_log("Waiting for ROI selection...")
+    listener = mouse.Listener(on_click=on_click)
+    listener.start()
 
+
+def show_roi():
+    sct = mss.mss()
+    ms = mouse.Controller()
+    step = 0
     while True:
         sct_img = np.array(sct.grab(bounding_box))
         cv2.imshow('ROI', sct_img)
 
-        # sct_img = cv2.resize(sct_img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-        # kernel = np.ones((2, 2), np.uint8)
-        # sct_img = cv2.dilate(sct_img, kernel, iterations=1)
-        # sct_img = cv2.erode(sct_img, kernel, iterations=1)
-        # sct_img = cv2.cvtColor(sct_img, cv2.COLOR_BGR2GRAY)
-        # sct_img = cv2.threshold(sct_img, 160, 255, cv2.THRESH_BINARY)[1]
-        # cv2.imshow('ROI2', sct_img)
-
-        image_text = pytesseract.image_to_string(sct_img, config='--psm 6')
-        # print(image_text)
+        image_text = pytesseract.image_to_string(sct_img)
         url_start_idx = image_text.find("https")
         if url_start_idx != -1:
             url = image_text[url_start_idx:image_text.find("viewform") + 8].replace("\n", "").replace(" ", "")
             print("URL detected: " + url)
-            # webbrowser.open(url)
 
-            pyautogui.click(bounding_box["left"] + int(bounding_box["width"] / 2), bounding_box["top"])
+            ms.position = (bounding_box["left"] + int(bounding_box["width"] / 3), bounding_box["top"] + step)
+            ms.click(mouse.Button.left, 1)
+            step += 10
+        else:
+            step = 0
 
-        if (cv2.waitKey(1) & 0xFF) == ord('q'):
+        if (cv2.waitKey(1) & 0xFF) == ord('q') or btn_select_roi["state"] == tk.DISABLED:
             cv2.destroyAllWindows()
             break
 
@@ -54,6 +75,9 @@ def on_closing():
     window.destroy()
 
 
+# Global
+bounding_box = {'left': 0, 'top': 0, 'width': 0, 'height': 0}
+
 # GUI window
 window = tk.Tk()
 window.title("Auto Sign In")
@@ -63,8 +87,8 @@ window.protocol("WM_DELETE_WINDOW", on_closing)
 # Frame frm_roi
 frm_roi = tk.Frame()
 frm_roi.pack(anchor="w", padx=10, pady=(10, 0))
-btn_play = tk.Button(master=frm_roi, text="Select ROI", width=10, command=select_roi)
-btn_play.pack(side=tk.LEFT)
+btn_select_roi = tk.Button(master=frm_roi, text="Select ROI", width=10, command=select_roi)
+btn_select_roi.pack(side=tk.LEFT)
 
 # Frame frm_log
 frm_log = tk.Frame()
