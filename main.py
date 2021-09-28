2060bae1e1fa78c5faa3dbf34fc239724ec84255
Samuel@MBP_BootCamp <oldchang0507@gmail.com>
2021-09-28 13:47:33 +0800

    Autofill by fixd_tabbing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 import cv2
 import mss
 import mss.tools
-from pynput import mouse
+from pynput import mouse, keyboard
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = "C:\\Sam\\Program_Files\\Tesseract-OCR\\tesseract.exe"
 # pytesseract.pytesseract.tesseract_cmd = "D:\\Sam\\Program_files\\Tesseract-OCR\\tesseract.exe"
@@ -41,27 +41,63 @@ def select_roi():
 
 def show_roi():
     sct = mss.mss()
-    ms = mouse.Controller()
+    url = ""
     step = 0
     while True:
         sct_img = np.array(sct.grab(bounding_box))
         cv2.imshow('ROI', sct_img)
 
+        if (cv2.waitKey(1) & 0xFF) == ord('q') or btn_select_roi["state"] == tk.DISABLED:
+            cv2.destroyAllWindows()
+            break
+
         image_text = pytesseract.image_to_string(sct_img)
-        url_start_idx = image_text.find("https")
+        url_start_idx = image_text.lower().find("https")
         if url_start_idx != -1:
-            url = image_text[url_start_idx:image_text.find("viewform") + 8].replace("\n", "").replace(" ", "")
-            print("URL detected: " + url)
+            url = image_text[url_start_idx:image_text[url_start_idx:].find("\n\n")].replace("\n", "").replace(" ", "")
+            if url.lower() in url_history:
+                continue
 
             ms.position = (bounding_box["left"] + int(bounding_box["width"] / 3), bounding_box["top"] + step)
             ms.click(mouse.Button.left, 1)
             step += 10
-        else:
+            if step >= bounding_box["height"]:
+                print_log("Invalid URL: " + url)
+                url_history.append(url.lower())
+                if len(url_history) > 3:
+                    url_history.pop(0)
+                step = 0
+        elif step != 0:
+            print_log("URL: " + url)
+            url_history.append(url.lower())
+            if len(url_history) > 3:
+                url_history.pop(0)
             step = 0
 
-        if (cv2.waitKey(1) & 0xFF) == ord('q') or btn_select_roi["state"] == tk.DISABLED:
-            cv2.destroyAllWindows()
-            break
+            time.sleep(3)
+            fixed_tabbing()
+
+
+def fixed_tabbing():
+    kb.press(keyboard.Key.tab)
+    kb.release(keyboard.Key.tab)
+    kb.press(keyboard.Key.tab)
+    kb.release(keyboard.Key.tab)
+    kb.press(keyboard.Key.tab)
+    kb.release(keyboard.Key.tab)
+    kb.type(ent_id_val.get())
+    kb.press(keyboard.Key.tab)
+    kb.release(keyboard.Key.tab)
+    kb.type(ent_name_val.get())
+    kb.press(keyboard.Key.tab)
+    kb.release(keyboard.Key.tab)
+    kb.press(keyboard.Key.enter)
+    kb.release(keyboard.Key.enter)
+    time.sleep(3)
+
+    with kb.pressed(keyboard.Key.ctrl):
+        kb.press("w")
+        kb.release("w")
 
 
 def print_log(msg):
@@ -77,11 +113,14 @@ def on_closing():
 
 # Global
 bounding_box = {'left': 0, 'top': 0, 'width': 0, 'height': 0}
+url_history = []
+ms = mouse.Controller()
+kb = keyboard.Controller()
 
 # GUI window
 window = tk.Tk()
 window.title("Auto Sign In")
-window.minsize(400, 300)
+window.minsize(800, 400)
 window.protocol("WM_DELETE_WINDOW", on_closing)
 
 # Frame frm_roi
@@ -89,11 +128,21 @@ frm_roi = tk.Frame()
 frm_roi.pack(anchor="w", padx=10, pady=(10, 0))
 btn_select_roi = tk.Button(master=frm_roi, text="Select ROI", width=10, command=select_roi)
 btn_select_roi.pack(side=tk.LEFT)
+lbl_id = tk.Label(master=frm_roi, text="ID:")
+lbl_id.pack(side=tk.LEFT, padx=(10, 0))
+ent_id_val = tk.StringVar(value="B0229050")
+ent_id = tk.Entry(master=frm_roi, width=20, textvariable=ent_id_val)
+ent_id.pack(side=tk.LEFT)
+lbl_name = tk.Label(master=frm_roi, text="Name:")
+lbl_name.pack(side=tk.LEFT, padx=(10, 0))
+ent_name_val = tk.StringVar(value="Samuel Chang")
+ent_name = tk.Entry(master=frm_roi, width=20, textvariable=ent_name_val)
+ent_name.pack(side=tk.LEFT)
 
 # Frame frm_log
 frm_log = tk.Frame()
 frm_log.pack(anchor="w", padx=10, pady=(10, 10), expand=True, fill=tk.BOTH)
-txt_log = tk.Text(master=frm_log, wrap=tk.WORD, width=10, height=10)
+txt_log = tk.Text(master=frm_log, wrap=tk.WORD, width=10, height=10, font="Consolas 8")
 txt_log.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
 txt_log.bind("<Key>", lambda e: "break")
 scb_log = tk.Scrollbar(master=frm_log, command=txt_log.yview)
